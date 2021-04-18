9bdd94c6969e7bee8df867c0c26e3dd8df86abd0
Griefer_gif <renato1diana@hotmail.com>
2021-04-18 20:28:16 -0300

    maggie change

--- Items/Weapons/Guns/Maggie.cs
+++ Items/Weapons/Guns/Maggie.cs
@@ -28,16 +28,16 @@ namespace Idkmod.Items.Weapons.Guns
 			item.ranged = true; // sets the damage type to ranged
 			item.width = 20; // hitbox width of the item
 			item.height = 20; // hitbox height of the item
-			item.useTime = 5; // The item's use time in ticks (60 ticks == 1 second.)
-			item.useAnimation = 10; // The length of the item's use animation in ticks (60 ticks == 1 second.)
-			item.reuseDelay = 15;
+			item.useTime = 2; // The item's use time in ticks (60 ticks == 1 second.)
+			item.useAnimation = 4; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			item.reuseDelay = 4;
 			item.useStyle = ItemUseStyleID.HoldingOut; // how you use the item (swinging, holding out, etc)
 			item.noMelee = true; //so the item's animation doesn't do damage
 			item.knockBack = 4; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
 			item.value = 10000; // how much the item sells for (measured in copper)
 			item.rare = ItemRarityID.Green; // the color that the item's name will be in-game
 			item.UseSound = SoundID.Item11; // The sound that this item plays when used.
-			item.autoReuse = true; // if you can hold click to automatically use it again
+			item.autoReuse = false; // if you can hold click to automatically use it again
 			item.shoot = ProjectileID.PurificationPowder; //idk why but all the guns in the vanilla source have this
 			item.shootSpeed = 16f; // the speed of the projectile (measured in pixels per frame)
 			item.useAmmo = AmmoID.Bullet; // The "ammo Id" of the ammo item that this weapon uses. Note that this is not an item Id, but just a magic value.
