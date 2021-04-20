6f23d04d425e60448657d88981397e870bd866b5
Griefer_gif <renato1diana@hotmail.com>
2021-04-20 11:30:07 -0300

    Maquina agricola

---
+++ Buffs/MAMount.cs
@@ -0,0 +1,22 @@
+﻿using Terraria;
+using Terraria.ModLoader;
+
+namespace idkmod.Buffs
+{
+	public class MAMount : ModBuff
+	{
+		public override void SetDefaults()
+		{
+			DisplayName.SetDefault("Maquina Agricola");
+			Description.SetDefault("poderosa");
+			Main.buffNoTimeDisplay[Type] = true;
+			Main.buffNoSave[Type] = true;
+		}
+
+		public override void Update(Player player, ref int buffIndex)
+		{
+			player.mount.SetMount(ModContent.MountType<Mounts.MaquinaAgricola>(), player);
+			player.buffTime[buffIndex] = 10;
+		}
+	}
+}
---
+++ Items/MaquinaAgricola.cs
@@ -0,0 +1,38 @@
+﻿using idkmod.Mounts;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace idkmod.Items
+{
+	public class MaquinaAgricola : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Chave da Maquina Agricola");
+			Tooltip.SetDefault("Maquina agricola");
+		}
+
+		public override void SetDefaults()
+		{
+			item.width = 20;
+			item.height = 30;
+			item.useTime = 20;
+			item.useAnimation = 20;
+			item.useStyle = ItemUseStyleID.SwingThrow;
+			item.value = 30000;
+			item.rare = ItemRarityID.Green;
+			item.UseSound = SoundID.Item79;
+			item.noMelee = true;
+			item.mountType = ModContent.MountType<Mounts.MaquinaAgricola>();
+		}
+
+		//public override void AddRecipes()
+		//{
+		//	ModRecipe recipe = new ModRecipe(mod);
+		//	recipe.AddIngredient(ModContent.ItemType<ExampleItem>(), 10);
+		//	recipe.AddTile(ModContent.TileType<ExampleWorkbench>());
+		//	recipe.SetResult(this);
+		//	recipe.AddRecipe();
+		//}
+	}
+}
