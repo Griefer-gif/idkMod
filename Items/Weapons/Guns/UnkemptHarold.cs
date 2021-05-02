85d80f4ef7ec3cb85f8c097da8860abb920e37fc
Griefer_gif <renato1diana@hotmail.com>
2021-05-01 22:46:10 -0300

    added Unkempt Harold, cool stuff

---
+++ Items/Weapons/Guns/UnkemptHarold.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria;
+using Terraria.ID;
+using Microsoft.Xna.Framework;
+
+namespace Idkmod.Items.Weapons.Guns
+{
+	class UnkemptHarold : ModItem
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Unkempt Harold");
+			Tooltip.SetDefault("fires 3 shots that split into two more shots that split again");
+		}
+
+		public override void SetDefaults()
+		{
+			item.damage = 50; // Sets the item's damage. Note that projectiles shot by this weapon will use its and the used ammunition's damage added together.
+			item.ranged = true; // sets the damage type to ranged
+			item.width = 40; // hitbox width of the item
+			item.height = 20; // hitbox height of the item
+			item.useTime = 1; // The item's use time in ticks (60 ticks == 1 second.)
+			item.useAnimation = 1; // The length of the item's use animation in ticks (60 ticks == 1 second.)
+			item.reuseDelay = 30;
+			item.useStyle = ItemUseStyleID.HoldingOut; // how you use the item (swinging, holding out, etc)
+			item.noMelee = true; //so the item's animation doesn't do damage
+			item.knockBack = 4; // Sets the item's knockback. Note that projectiles shot by this weapon will use its and the used ammunition's knockback added together.
+			item.value = 10000; // how much the item sells for (measured in copper)
+			item.rare = ItemRarityID.Quest; // the color that the item's name will be in-game
+			item.UseSound = SoundID.Item11; // The sound that this item plays when used.
+			item.autoReuse = true; // if you can hold click to automatically use it again
+			item.shoot = ProjectileID.PurificationPowder; //idk why but all the guns in the vanilla source have this
+			item.shootSpeed = 60f; //og:16f // the speed of the projectile (measured in pixels per frame)
+			item.useAmmo = AmmoID.Bullet;
+
+		}
+
+		public override bool Shoot(Player player, ref Vector2 position, ref float speedX, ref float speedY, ref int type, ref int damage, ref float knockBack)
+		{
+			type = ModContent.ProjectileType<idkmod.Projectiles.Harold.Harold1>();
+			float numberProjectiles = 3;
+			float rotation = MathHelper.ToRadians(2);
+			position += Vector2.Normalize(new Vector2(speedX, speedY)) * 45f;
+			for (int i = 0; i < numberProjectiles; i++)
+			{
+				Vector2 perturbedSpeed = new Vector2(speedX, speedY).RotatedBy(MathHelper.Lerp(-rotation, rotation, i / (numberProjectiles - 1))) * .1f; // Watch out for dividing by 0 if there is only 1 projectile.
+				Projectile.NewProjectile(position.X, position.Y, perturbedSpeed.X, perturbedSpeed.Y, type, damage, knockBack, player.whoAmI);
+			}
+			return false;
+		}
+
+		public override Vector2? HoldoutOffset()
+		{
+			return new Vector2(-8, 3);
+		}
+
+		public override void ModifyTooltips(List<TooltipLine> tooltips)
+		{
+			//tooltips.Add(new TooltipLine(mod, "", "Ignores ammo type"));
+			var quote = new TooltipLine(mod, "", "'Let's just ping everyone all at once.'");
+			quote.overrideColor = Color.Red;
+			tooltips.Add(quote);
+		}
+	}
+}
---
+++ Projectiles/Harold/HaroldEnd.cs
@@ -0,0 +1,92 @@
+﻿using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+using System;
+using Terraria;
+using Terraria.ID;
+using Terraria.ModLoader;
+
+namespace idkmod.Projectiles.Harold
+{
+	public class HaroldEnd : ModProjectile
+	{
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("CCBullet");     //The English name of the projectile
+			ProjectileID.Sets.TrailCacheLength[projectile.type] = 10;    //The length of old position to be recorded
+			ProjectileID.Sets.TrailingMode[projectile.type] = 0;        //The recording mode
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 8;               //The width of projectile hitbox
+			projectile.height = 8;              //The height of projectile hitbox
+			projectile.aiStyle = 1;             //The ai style of the projectile, please reference the source code of Terraria
+			projectile.friendly = true;         //Can the projectile deal damage to enemies?
+			projectile.hostile = false;         //Can the projectile deal damage to the player?
+			projectile.ranged = true;           //Is the projectile shoot by a ranged weapon?
+			projectile.penetrate = 20;           //How many monsters the projectile can penetrate. (OnTileCollide below also decrements penetrate for bounces as well)
+			projectile.timeLeft = 600; //og: 10         //The live time for the projectile (60 = 1 second, so 600 is 10 seconds)
+			projectile.alpha = 255;             //The transparency of the projectile, 255 for completely transparent. (aiStyle 1 quickly fades the projectile in) Make sure to delete this if you aren't using an aiStyle that fades in. You'll wonder why your projectile is invisible.
+			projectile.light = 0.5f;            //How much light emit around the projectile
+			projectile.ignoreWater = true;          //Does the projectile's speed be influenced by water?
+			projectile.tileCollide = true;          //Can the projectile collide with tiles?
+			projectile.extraUpdates = 1;            //Set to above 0 if you want the projectile to update multiple time in a frame
+			aiType = ProjectileID.Bullet;           //Act exactly like default Bullet
+			projectile.penetrate = 1;
+
+		}
+		public override bool PreDraw(SpriteBatch spriteBatch, Color lightColor)
+		{
+			//Redraw the projectile with the color not influenced by light
+			//Vector2 drawOrigin = new Vector2(Main.projectileTexture[projectile.type].Width * 0.5f, projectile.height * 0.5f);
+			//for (int k = 0; k < projectile.oldPos.Length; k++)
+			//{
+			//	Vector2 drawPos = projectile.oldPos[k] - Main.screenPosition + drawOrigin + new Vector2(0f, projectile.gfxOffY);
+			//	Color color = projectile.GetAlpha(lightColor) * ((float)(projectile.oldPos.Length - k) / (float)projectile.oldPos.Length);
+			//	spriteBatch.Draw(Main.projectileTexture[projectile.type], drawPos, null, color, projectile.rotation, drawOrigin, projectile.scale, SpriteEffects.None, 0f);
+			//}
+
+			int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, 55, 0f, 0f, 10, default(Color), 1f);
+			Main.dust[dustIndex].noGravity = true;
+
+			return true;
+		}
+
+		public override void OnHitNPC(NPC target, int damage, float knockback, bool crit)
+		{
+			for (int i = 0; i < 25; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, DustID.Smoke, 0f, 0f, 10, default(Color), 1f);
+				Main.dust[dustIndex].velocity *= 1f;
+			}
+			// Fire Dust spawn
+			for (int i = 0; i < 40; i++)
+			{
+				int dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, DustID.Fire, 0f, 0f, 10, default(Color), 1.6f);
+				Main.dust[dustIndex].noGravity = true;
+				Main.dust[dustIndex].velocity *= 1f;
+				dustIndex = Dust.NewDust(new Vector2(projectile.position.X, projectile.position.Y), projectile.width, projectile.height, DustID.Fire, 0f, 0f, 10, default(Color), 1.6f);
+				Main.dust[dustIndex].velocity *= 1f;
+				//Main.dust[dustIndex].noGravity = true;
+			}
+
+			// reset size to normal width and height.
+			projectile.position.X = projectile.position.X + (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y + (float)(projectile.height / 2);
+			projectile.width = 10;
+			projectile.height = 10;
+			projectile.position.X = projectile.position.X - (float)(projectile.width / 2);
+			projectile.position.Y = projectile.position.Y - (float)(projectile.height / 2);
+
+			Main.PlaySound(SoundID.DD2_ExplosiveTrapExplode, projectile.position);
+		}
+
+		public override void Kill(int timeLeft)
+		{
+			// This code and the similar code above in OnTileCollide spawn dust from the tiles collided with. SoundID.Item10 is the bounce sound you hear.
+			Collision.HitTiles(projectile.position + projectile.velocity, projectile.velocity, projectile.width, projectile.height);
+			Main.PlaySound(SoundID.Item10, projectile.position);
+		}
+
+	}
+}
