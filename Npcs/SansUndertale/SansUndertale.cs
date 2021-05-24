7883f99c4ef7b6694e988f8304f43909bfa36aab
Griefer_gif <renato1diana@hotmail.com>
2021-05-24 20:42:10 -0300

    Snads undertail, yep

---
+++ Npcs/SansUndertale/SansUndertale.cs
@@ -0,0 +1,48 @@
+﻿using Terraria;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Terraria.ModLoader;
+using Terraria.ID;
+
+namespace Idkmod.Npcs.SansUndertale
+{
+	public class SansUndertale : ModNPC
+	{
+
+
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("The skeleton");
+			//Main.npcFrameCount[npc.type] = 2;
+		}
+
+		public override void SetDefaults()
+		{
+			npc.aiStyle = -1;
+			npc.lifeMax = 40000;
+			npc.damage = 100;
+			npc.defense = 55;
+			npc.knockBackResist = 0f;
+			npc.width = 50;
+			npc.height = 50;
+			npc.value = Item.buyPrice(0, 20, 0, 0);
+			npc.npcSlots = 15f;
+			npc.boss = true;
+			npc.lavaImmune = true;
+			npc.noGravity = true;
+			npc.noTileCollide = true;
+			npc.HitSound = SoundID.NPCHit1;
+			npc.DeathSound = SoundID.NPCDeath1;
+			//npc.buffImmune[24] = true;
+			music = MusicID.Boss2;
+		}
+
+        public override void AI()
+        {
+			npc.position = Main.player[0].position;
+        }
+    }
+}
