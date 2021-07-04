95096430d9a121c1abaa6037672011ffaf306598
Griefer_gif <renato1diana@hotmail.com>
2021-07-04 00:47:14 -0300

    hand kindof done, but its trash if multiple bosses are spanwed, so prevent that

--- Npcs/TitanSoulsBoss/TheTitan.cs
+++ Npcs/TitanSoulsBoss/TheTitan.cs
@@ -10,7 +10,7 @@ using Terraria.ModLoader;
 
 namespace Idkmod.Npcs.TitanSoulsBoss
 {
-    class TitanMainBody : ModNPC
+    class TheTitan : ModNPC
     {
 		//--------------------------------------------------//idea//----------------------------------------------------------
 		//imma write the whole boss idea here because yes
@@ -22,7 +22,7 @@ namespace Idkmod.Npcs.TitanSoulsBoss
 		//------------------------------------------------------------------------------------------------------------------
         public override void SetStaticDefaults()
         {
-            DisplayName.SetDefault("The Titan"); // Automatic from .lang files
+            DisplayName.SetDefault("The Titan"); 
             Main.npcFrameCount[npc.type] = 6; // make sure to set this for your modnpcs.
         }
 
@@ -34,6 +34,7 @@ namespace Idkmod.Npcs.TitanSoulsBoss
 			npc.damage = 7;
 			npc.defense = 2;
 			npc.lifeMax = 25;
+			npc.friendly = false;
 			npc.HitSound = SoundID.NPCHit1;
 			npc.DeathSound = SoundID.NPCDeath1;
 			npc.value = 25f;
@@ -92,6 +93,7 @@ namespace Idkmod.Npcs.TitanSoulsBoss
 					for(int i = 0; i < 50; i++)
                     {
 						Vector2 DustPos = new Vector2(npc.position.X, npc.position.Y + 16f);
+						Projectile.NewProjectile(npc.Center - new Vector2(40, 10), Vector2.Zero, ModContent.ProjectileType<LeftHand>(), 100, 10);
 
 						Dust.NewDust(DustPos, 5, 1, DustID.Smoke, 5, 0);
 						Dust.NewDust(DustPos, 5, 5, DustID.Smoke, -5, 0);
@@ -112,9 +114,94 @@ namespace Idkmod.Npcs.TitanSoulsBoss
 				targetHoverPos = new Vector2(target.position.X, target.position.Y - 80f) - npc.Center;
 				targetHoverPos.Normalize();
 				targetHoverPos *= NPCspeed;
-				npc.velocity = (npc.velocity * (NPCinertia - 1) + targetHoverPos) / NPCinertia;
+				 npc.velocity = (npc.velocity * (NPCinertia - 1) + targetHoverPos) / NPCinertia;
 
 			}
         }
     }
+
+	class LeftHand : ModProjectile
+    {
+		public override void SetStaticDefaults()
+		{
+			DisplayName.SetDefault("Titan left hand");
+		}
+
+		public override void SetDefaults()
+		{
+			projectile.width = 32;
+			projectile.height = 32;
+			projectile.aiStyle = -1; 
+			projectile.damage = 100;
+			projectile.tileCollide = false;
+			projectile.penetrate = 99999;
+			projectile.hostile = false;
+			projectile.friendly = false;
+		}
+
+
+		//what the ai[] positions are used for
+		private const int AI_State_Slot = 0;
+		private const int AI_Owner_Slot = 1;
+		private const int AI_Timer_Slot = 2;
+		private const int AI_Unused_Slot = 3;
+
+		//yep
+		private const int Local_AI_Unused_Slot_0 = 0;
+		private const int Local_AI_Unused_Slot_1 = 1;
+		private const int Local_AI_Unused_Slot_2 = 2;
+		private const int Local_AI_Unused_Slot_3 = 3;
+
+		//the state of the projectile that goes into ai[]
+		private const int State_HoveringAtSide = 0;
+		private const int State_HoveringOverTarget = 1;
+		private const int State_Stomping = 2;
+
+		public float AI_State
+		{
+			get => projectile.ai[AI_State_Slot];
+			set => projectile.ai[AI_State_Slot] = value;
+		}
+
+		public float AI_Timer
+		{
+			get => projectile.ai[AI_Timer_Slot];
+			set => projectile.ai[AI_Timer_Slot] = value;
+		}
+
+		public NPC AI_HandOwner
+		{
+			get => Main.npc[(int)projectile.ai[AI_Owner_Slot]];
+		}
+
+		Vector2 HoverPos = Vector2.Zero;
+		const float speed = 7f;
+		const float inertia = 10f;
+		public override void AI()
+		{
+			for(int i = 0; i < Main.npc.Count(); i++)
+            {
+				NPC cNPC = Main.npc[i];
+				if (!cNPC.friendly && cNPC.type == ModContent.NPCType<TheTitan>() && cNPC.active)
+                {
+					HoverPos = cNPC.Center - new Vector2(50, 0);
+                }
+            }
+			HoverPos = AI_HandOwner.Center - new Vector2(50, 0);
+			if (!AI_HandOwner.active)
+            {
+				projectile.active = false;
+            }
+
+			if(AI_State == State_HoveringAtSide)
+            {
+				projectile.hostile = false;
+				Vector2 direction = HoverPos - projectile.Center;
+				//direction.X -= 40f;
+				direction.Normalize();
+				direction *= speed;
+				projectile.velocity = (projectile.velocity * (inertia - 1) + direction) / inertia;
+			}
+		}
+	}
 }
